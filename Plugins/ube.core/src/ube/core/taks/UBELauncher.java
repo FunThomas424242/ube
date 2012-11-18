9f4b56dd122c0e446fb0a52781dcb4c805e4ab22
Thomas Schubert <funthomas424242@googlemail.com>
2012-11-18 18:19:38 +0100

    initial test

---
+++ Plugins/ube.core/src/ube/core/taks/UBELauncher.java
@@ -0,0 +1,10 @@
+package ube.core.taks;
+
+public class UBELauncher {
+
+	public void execute(){
+		// Launch the UBE System
+		System.out.println("Hallo UBE");
+		// Read the ube build files
+	}
+}
