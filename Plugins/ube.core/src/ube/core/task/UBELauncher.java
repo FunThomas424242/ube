29ec6a18f11d51b81d362bd04016de4deb3abf3f
Thomas Schubert <funthomas424242@googlemail.com>
2012-11-20 21:42:16 +0100

    Dependency to ant added

--- Plugins/ube.core/src/ube/core/task/UBELauncher.java
+++ Plugins/ube.core/src/ube/core/task/UBELauncher.java
@@ -1,8 +1,27 @@
 package ube.core.task;
 
-public class UBELauncher {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-	public void execute(){
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.condition.Condition;
+
+public class UBELauncher extends Task {
+
+	private List conditions = new ArrayList();
+
+	public void add(Condition c) {
+		conditions.add(c);
+	}
+
+	public void execute() {
+
+		for (Iterator iterator = conditions.iterator(); iterator.hasNext();) {
+			Condition condition = (Condition) iterator.next();
+			System.out.println("Condition:" + condition.toString());
+
+		}
 		// Launch the UBE System
 		System.out.println("Hallo UBE");
 		// Read the ube build files
