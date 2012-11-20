0fdfcc897644985dcc626fb5dcfa9f3bf933564f
Thomas Schubert <funthomas424242@googlemail.com>
2012-11-20 22:21:09 +0100

    condition example fixed

--- Plugins/ube.core/src/ube/core/task/UBELauncher.java
+++ Plugins/ube.core/src/ube/core/task/UBELauncher.java
@@ -9,21 +9,30 @@ import org.apache.tools.ant.taskdefs.condition.Condition;
 
 public class UBELauncher extends Task {
 
-	private List conditions = new ArrayList();
+	private final List<Condition> conditions = new ArrayList<Condition>();
 
 	public void add(Condition c) {
 		conditions.add(c);
 	}
 
+	@Override
 	public void execute() {
 
-		for (Iterator iterator = conditions.iterator(); iterator.hasNext();) {
-			Condition condition = (Condition) iterator.next();
-			System.out.println("Condition:" + condition.toString());
-
+		boolean areTrue = true;
+		for (Iterator<Condition> iterator = conditions.iterator(); iterator
+				.hasNext();) {
+			Condition condition = iterator.next();
+			areTrue = areTrue && condition.eval();
+			System.out.println("Condition:" + condition.eval());
+		}
+		if (areTrue) {
+			System.out.println("Conditions are TRUE");
+		} else {
+			System.out.println("Any Conditions are FALSE");
 		}
 		// Launch the UBE System
 		System.out.println("Hallo UBE");
 		// Read the ube build files
+
 	}
 }
