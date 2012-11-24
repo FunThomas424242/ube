d6c16849683a2bd8e2b79f763d39cb060d912fb1
Thomas Schubert <funthomas424242@googlemail.com>
2012-11-24 21:12:58 +0100

    subproject ube.dsl added

--- Plugins/ube.dsl/metamodel/ube.cs
+++ Plugins/ube.dsl/metamodel/ube.cs
@@ -2,6 +2,19 @@ SYNTAXDEF ube
 FOR <http://www.emftext.org/language/ube>
 START EntityModel
 
+OPTIONS{
+ reloadGeneratorModel="true";
+ generateCodeFromGeneratorModel="true"; //wichtig um Fehler im Generat zu vermeiden
+ disableLaunchSupport="false";
+ //usePredefinedTokens="true";
+ resourcePluginID="ube.dsl.resource";
+ resourceUIPluginID="ube.dsl.ui";
+ basePackage="ube.dsl.resource";
+ //srcFolder="src";
+ //srcGenFolder="src-gen";
+ //uiSrcFolder="src";
+ //uiSrcGenFolder="src-gen";
+}
 
 RULES {
 	EntityModel ::= "model" types*;
